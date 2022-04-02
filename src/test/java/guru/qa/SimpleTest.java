3c0d89a62929265a790c5f556c3920f25f6091f3
Tatiana Komar <tatiana23komar@gmail.com>
2022-04-02 23:43:27 +0300

    add BeforeAll

--- src/test/java/guru/qa/SimpleTest.java
+++ src/test/java/guru/qa/SimpleTest.java
@@ -1,4 +1,13 @@
 package guru.qa;
 
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
 public class SimpleTest {
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.browserSize = "1920x1080";
 }
