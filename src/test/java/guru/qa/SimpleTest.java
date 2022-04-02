41051c54f07de812903b69b6009f73622718ce58
Tatiana Komar <tatiana23komar@gmail.com>
2022-04-02 23:54:43 +0300

    add some...

--- src/test/java/guru/qa/SimpleTest.java
+++ src/test/java/guru/qa/SimpleTest.java
@@ -3,11 +3,37 @@ package guru.qa;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class SimpleTest {
     @BeforeAll
     static void setUp() {
         Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+}
+
+    @Test
+    void fillFormTest() {
+
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("Alena");
+        $("[id=lastName]").setValue("Arbuzova");
+        $("[id=userEmail]").setValue("alena@arbuzova.com");
+        $(byText("Female")).click();
+        $("[id=userNumber]").setValue("1234567890");
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
+        $("[id=subjectsInput]").sendKeys("a");
+        $(byText("Arts")).click();
+        $("[id=currentAddress]").setValue("Some street");
+
+    }
 }
