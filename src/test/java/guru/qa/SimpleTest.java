3438ca978a0382429d123428d31211053f2a388e
Tatiana Komar <tatiana23komar@gmail.com>
2022-04-03 22:31:13 +0300

    add full code. done

--- src/test/java/guru/qa/SimpleTest.java
+++ src/test/java/guru/qa/SimpleTest.java
@@ -1,14 +1,13 @@
 package guru.qa;
 
-
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-
 
 public class SimpleTest {
     @BeforeAll
@@ -23,17 +22,35 @@ public class SimpleTest {
 
         open("/automation-practice-form");
 
+        //скрывает баннер и футер
+        Selenide.executeJavaScript("document.querySelector(\"footer\").hidden = 'true';" +
+                "document.querySelector(\"#fixedban\").hidden = 'true'");
+
         $("[id=firstName]").setValue("Alena");
         $("[id=lastName]").setValue("Arbuzova");
         $("[id=userEmail]").setValue("alena@arbuzova.com");
         $(byText("Female")).click();
         $("[id=userNumber]").setValue("1234567890");
+
+        $("[id=dateOfBirthInput]").click();
+        $("[class=react-datepicker__month-select]").selectOption("February");
+        $("[class=react-datepicker__year-select]").selectOption("1984");
+        $(byText("15")).click();
+
+        $("[id=subjectsInput]").sendKeys("a");
+        $(byText("Arts")).click();
+
         $(byText("Sports")).click();
         $(byText("Reading")).click();
         $(byText("Music")).click();
-        $("[id=subjectsInput]").sendKeys("a");
-        $(byText("Arts")).click();
-        $("[id=currentAddress]").setValue("Some street");
+        $("[id=uploadPicture]").uploadFromClasspath("photo.jpg");
 
+        $("[id=currentAddress]").setValue("Some street");
+        $("[id=state]").click();
+        $(byText("NCR")).click();
+        $("[id=city]").click();
+        $(byText("Delhi")).click();
+
+        $("[id=submit]").click();
     }
 }
