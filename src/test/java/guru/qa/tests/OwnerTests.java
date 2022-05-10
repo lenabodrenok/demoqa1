7d623bbd9d280a118f110a7b943ba6f20fa18420
Elena <lena.bodrenok@gmail.com>
2022-05-10 22:42:25 +0300

    add owner

---
+++ src/test/java/guru/qa/tests/OwnerTests.java
@@ -0,0 +1,17 @@
+package guru.qa.tests;
+
+import guru.qa.config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class OwnerTests {
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+        @Test
+        @Tag("owner")
+        void loginTest() {
+            String login = config.login();
+            String password = config.password();
+        }
+}
